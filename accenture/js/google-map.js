42ba26eda4bd5ff9baa706ed47e0f2f8b3e67fb9
dbakti7 <dbakti1605@gmail.com>
2016-02-14 01:55:35 +0800

    search text box

---
+++ accenture/js/google-map.js
@@ -0,0 +1,52 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'));
+    var input = /** @type {!HTMLInputElement} */(
+        document.getElementById('pac-input'));
+
+    var types = document.getElementById('type-selector');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    var infowindow = new google.maps.InfoWindow();
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+
+        var place = autocomplete.getPlace(); // store the place details
+
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        //infowindow.open(map, marker);
+    });
+
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, types) {
+        var radioButton = document.getElementById(id);
+        radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+        });
+    }
+
+    setupClickListener('changetype-all', []);
+    setupClickListener('changetype-address', ['address']);
+    setupClickListener('changetype-establishment', ['establishment']);
+    setupClickListener('changetype-geocode', ['geocode']);
+}/**
+ * Created by dbakti7 on 2/14/2016.
+ */
